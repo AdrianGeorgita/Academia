dd5bc8c11709f29c9bd1bf105c64c81b6e41f3c5
Adrian Georgita <adrian.georgita@student.tuiasi.ro>
2024-11-15 13:41:18 +0200

    Adaugat posibilitatea de a modifica date despre o disciplina

--- LecturesWebService/main.py
+++ LecturesWebService/main.py
@@ -1,6 +1,8 @@
 import json
 
 from fastapi import FastAPI
+from fastapi import Request, HTTPException
+from bson.json_util import dumps
 import pymongo
 
 
@@ -68,12 +70,80 @@ async def root():
 
 @app.get("/materials")
 async def root():
-    materials = []
     collection = pos_collection.find({})
-    for doc in collection:
-        materials.append(doc)
+    materials = json.loads(dumps(collection))
+    return {"materials": materials}
 
-    print(materials)
+
+@app.get("/materials/{course}")
+async def root(course: str):
+    collection = pos_collection.find_one({"disciplina": course})
+    materials = json.loads(dumps(collection))
+    return {"materials": materials}
+
+@app.put("/materials/{course}")
+async def root(course: str, request: Request):
+
+    if not pos_collection.find_one({"disciplina": course}):
+        raise HTTPException(status_code=404, detail="Course not found")
+
+    body = await request.json()
+
+    evaluation = body["probe-evaluare"]
+
+    # Validate Grade Percentages
+    percentage = 0
+    for evaluationType in evaluation:
+        if evaluation[evaluationType] < 0 or evaluation[evaluationType] > 1:
+            raise HTTPException(status_code=422, detail="Invalid Percentage")
+        percentage += evaluation[evaluationType]
+
+    if percentage != 1:
+        raise HTTPException(status_code=422, detail="Invalid Percentages")
+
+    # Validate Course Materials
+    course_materials = body["materiale-curs"]
+    paths = []
+    for course_material in course_materials:
+        # Validate name and path ?
+        if course_material["nume-fisier"] == "" or course_material["path"] == "":
+            raise HTTPException(status_code=422, detail="Invalid Path or Name")
+        if course_material["path"] in paths:
+            raise HTTPException(status_code=422, detail="Duplicate Path")
+        if len(course_material["path"]) > 2048:
+            raise HTTPException(status_code=422, detail="Path too long")
+        if len(course_material["nume-fisier"]) > 256:
+            raise HTTPException(status_code=422, detail="Name too long")
+        paths.append(course_material["path"])
+
+    # Validate Laboratory Materials
+    lab_materials = body["materiale-laborator"]
+    paths = []
+    for lab_material in lab_materials:
+        # Validate name and path ?
+        if lab_material["nume-fisier"] == "" or lab_material["path"] == "":
+            raise HTTPException(status_code=422, detail="Invalid Path or Name")
+        if lab_material["path"] in paths:
+            raise HTTPException(status_code=422, detail="Duplicate Path")
+        if len(lab_material["path"]) > 2048:
+            raise HTTPException(status_code=422, detail="Path too long")
+        if len(lab_material["nume-fisier"]) > 256:
+            raise HTTPException(status_code=422, detail="Name too long")
+        paths.append(lab_material["path"])
+
+    pos_collection.update_one(
+        {"disciplina": course},
+        {
+            "$set": {
+                "probe-evaluare": evaluation,
+                "materiale-curs": course_materials,
+                "materiale-laborator": lab_materials,
+            }
+        }
+    )
+
+    return {"message": "Course Updated"}
+
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
